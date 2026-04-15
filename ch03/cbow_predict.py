d7b8776ffa974a7fcae455402a2555ac7349a419
longxuebin <longxuebin@mistong.com>
2026-04-15 10:26:23 +0800

    加日志

--- ch03/cbow_predict.py
+++ ch03/cbow_predict.py
@@ -2,8 +2,7 @@
 import sys
 sys.path.append('..')
 import numpy as np
-from common.layers import MatMul
-
+from common.layers import MatMul, Softmax
 
 # 样本的上下文数据
 c0 = np.array([[1, 0, 0, 0, 0, 0, 0]])
@@ -17,10 +16,19 @@ W_out = np.random.randn(3, 7)
 in_layer0 = MatMul(W_in)
 in_layer1 = MatMul(W_in)
 out_layer = MatMul(W_out)
+#ToDo 自己的Softmax
+softmax_layer = Softmax()
 
 # 正向传播
 h0 = in_layer0.forward(c0)
 h1 = in_layer1.forward(c1)
 h = 0.5 * (h0 + h1)
 s = out_layer.forward(h)
+prob = softmax_layer.forward(s)
+
+#todo @Toby 自己修改的输出
+print("=== 原始得分（logits）===")
 print(s)
+print("\n=== Softmax 输出（概率分布）===")
+print(prob)
+print("\n=== 概率总和 =", np.sum(prob))  # 一定 = 1.0
